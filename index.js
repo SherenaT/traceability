337d8ed55b94ada7ed98b13808712f619fffa968
Sherena Tu <s.tu314@yahoo.com>
2021-08-31 15:06:43 -0700

    changes to index and js

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const rollbar = new Rollbar({
   captureUnhandledRejections: true,
 });
 
+const wishlist = [];
+globalId = 1;
+
 const app = express();
 app.use(express.json());
 app.use("/style", express.static("/public/styles.css"));
@@ -15,6 +18,30 @@ app.use("/style", express.static("/public/styles.css"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
   rollbar.info("html file served successfully.");
+});
+
+app.post("/api/wishlist", (req, res) => {
+  const { name, priority } = req.body;
+
+  let newItem = {
+    name,
+    priority,
+    id: globalId,
+  };
+
+  wishlist.push(newItem);
+
+  res.status(200).send(wishlist);
+  globalId++;
+});
+app.delete("/api/wishlist/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = wishlist.findIndex((e) => e.id === +id);
+
+  wishlist.splice(index, 1);
+
+  res.status(200).send(wishlist);
 });
 
 const port = process.env.PORT || 4000;
