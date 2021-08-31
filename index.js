070e4db5170daaaecbf9d423c49a05f07da1408c
Sherena Tu <s.tu314@yahoo.com>
2021-08-31 15:53:32 -0700

    changes to index and js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
   rollbar.info("html file served successfully.");
 });
 
-app.post("/api/student", (req, res) => {
+app.post("/api/waitlist", (req, res) => {
   let { name } = req.body;
   name = name.trim();
 
